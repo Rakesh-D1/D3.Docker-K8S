128972c0fb8f6ca01402728243096f7b8743283c
wsl-rakesh <you@example.com>
2026-02-04 14:20:09 +0000

    adding code.

---
+++ 6/82b.jenkins-learn/9.first-jenkins-pipeline-job/2.sh
@@ -0,0 +1,24 @@
+pipeline {  
+    agent any
+
+    stages {
+        stage('Build PC') {
+            steps {
+                echo 'we are building a new PC'
+                sh 'mkdir -p build-pc'
+                sh 'touch build-pc/computer.txt'
+                sh 'echo "motherboard" >> build-pc/computer.txt'
+                sh 'cat build-pc/computer.txt'
+                sh 'echo "display" >> build-pc/computer.txt'
+                sh 'cat build-pc/computer.txt'
+                sh 'echo "keyboard" >> build-pc/computer.txt'
+                sh 'cat build-pc/computer.txt'
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
---
+++ 6/82b.jenkins-learn/9.first-jenkins-pipeline-job/3.sh
@@ -0,0 +1,25 @@
+pipeline {  
+    agent any
+
+    stages {
+        stage('Build PC') {
+            steps {
+                cleanWs()
+                echo 'we are building a new PC'
+                sh 'mkdir -p build-pc'
+                sh 'touch build-pc/computer.txt'
+                sh 'echo "motherboard" >> build-pc/computer.txt'
+                sh 'cat build-pc/computer.txt'
+                sh 'echo "display" >> build-pc/computer.txt'
+                sh 'cat build-pc/computer.txt'
+                sh 'echo "keyboard" >> build-pc/computer.txt'
+                sh 'cat build-pc/computer.txt'
+            }
+        }
+    }
+    post {
+        success {
+            archiveArtifacts artifacts: 'build-pc/**'
+        }
+    }
+}
