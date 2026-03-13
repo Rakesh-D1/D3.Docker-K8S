71d82e0f42746944d6b71bef0dc88807c06c1c68
wsl-rakesh <you@example.com>
2026-03-13 15:33:29 +0000

    test

--- 6/82b.jenkins-install/jenkins-backup.sh
+++ 6/82b.jenkins-install/jenkins-backup.sh
@@ -1,4 +1,4 @@
 docker run --rm \
     -v 82ajenkins-install_jenkins-data:/data \
     -v $(pwd):/backup \
-    alpine tar czf /backup/jenkins-backup-$().tar.gz -C /data .
+    alpine tar czf /backup/jenkins-backup-$(date +%Y%m%d_%H%M%S).tar.gz -C /data .
